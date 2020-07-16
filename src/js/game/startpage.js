4a84d72038d45c1875eb0dde8d9705e96b109a92
SP12893678 <office1289366@gmail.com>
2020-07-16 13:51:29 +0800

    gasp game start page

--- src/js/game/startpage.js
+++ src/js/game/startpage.js
@@ -1,5 +1,10 @@
 import * as PIXI from 'pixi.js'
+import { gsap } from 'gsap'
+import { PixiPlugin } from 'gsap/PixiPlugin'
 // import load_resources from './assets/json/resources.json'
+
+gsap.registerPlugin(PixiPlugin)
+PixiPlugin.registerPIXI(PIXI)
 
 let Application = PIXI.Application,
     Container = PIXI.Container,
@@ -31,7 +36,19 @@ export default class GameStartUI {
             fill: 0xf1f1f1,
             align: 'center',
         })
-        text.position.set((this.width - text.width) / 2, ((this.height - text.height) / 4) * 3)
+        text.alpha = 0.5
+        text.anchor.set(0.5, 0.5)
+        text.position.set(this.width / 2, (this.height / 4) * 3)
         this.container.addChild(text)
+        this.doGASP(text)
+    }
+
+    doGASP(sprite) {
+        gsap.to(sprite, {
+            pixi: { scaleX: 1.5, scaleY: 1.5, alpha: 1, positionY: sprite.position.y + 15 },
+            duration: 1,
+            repeat: -1,
+            yoyo: true,
+        })
     }
 }
